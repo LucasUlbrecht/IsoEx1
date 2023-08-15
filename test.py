a0beffb8f96efc7ce502fe429e14ab6f0cf2a703
Lucas Ulbrecht <luke.patrizi.07@gmail.com>
2023-08-15 08:28:24 -0400

    teste gerador lista

---
+++ test.py
@@ -0,0 +1,9 @@
+import funcoes as funcoes
+vetor=funcoes.geradorLista.gerarVetorAleatorio(0,100,5)
+print(vetor)
+vetor=funcoes.geradorLista.gerarVetorOrdenado(0,100,5)
+print(vetor)
+vetor=funcoes.geradorLista.gerarVetorReverso(0,100,5)
+print(vetor)
+vetor=funcoes.geradorLista.gerarVetorQuaseOrdenado(0,100,5)
+print(vetor)
